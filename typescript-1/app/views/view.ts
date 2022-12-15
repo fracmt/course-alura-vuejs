c592f42d211a610ceee70af665808839f8aa6324
Francisco Torres <fracmt@gmail.com>
2022-12-15 00:06:30 +0000

    Check DOM elements exists and TS config

--- typescript-1/app/views/view.ts
+++ typescript-1/app/views/view.ts
@@ -2,7 +2,11 @@ export abstract class View<T> {
     protected element: HTMLElement;
 
     constructor(selector: string) {
-        this.element = document.querySelector(selector);
+        const element: Element = document.querySelector(selector);
+        if(!element) {
+            throw Error(`The selector ${selector} is invalid!`);
+        }
+        this.element = <HTMLElement>element;
     }
 
     protected abstract template(model: T): string;
