5bb47f14d4dbccdd8516b547aa91fea06dae63d5
Francisco Torres <fracmt@gmail.com>
2022-12-19 19:12:33 +0000

    create box and show message when task list empty

--- alura-tracker/src/App.vue
+++ alura-tracker/src/App.vue
@@ -4,9 +4,12 @@
 			<side-bar />
 		</div>
 		<div class="column is-three-quarter">
-			<task-form @toSaveTask="saveTask"/>
+			<task-form @toSaveTask="saveTask" />
 			<div class="taskList">
-				<task-item v-for="(taskItem, index) in tasks" :key="index" :task="taskItem"/>
+				<task-item v-for="(taskItem, index) in tasks" :key="index" :task="taskItem" />
+				<task-box v-if="isEmptyTaskList">
+					The task list is empty.
+				</task-box>
 			</div>
 		</div>
 	</main>
@@ -18,13 +21,19 @@ import SideBar from './components/SideBar.vue';
 import TaskForm from './components/TaskForm.vue';
 import TaskItem from './components/TaskItem.vue';
 import ITask from './interfaces/ITask';
+import TaskBox from './components/TaskBox.vue';
 
 export default defineComponent({
 	name: "App",
-	components: { SideBar, TaskForm, TaskItem },
+	components: { SideBar, TaskForm, TaskItem, TaskBox },
 	data() {
 		return {
 			tasks: [] as ITask[]
+		}
+	},
+	computed: {
+		isEmptyTaskList(): boolean {
+			return this.tasks.length === 0
 		}
 	},
 	methods: {
--- alura-tracker/src/components/TaskItem.vue
+++ alura-tracker/src/components/TaskItem.vue
@@ -1,22 +1,25 @@
 <template>
-	<div class="box has-text-weight-bold">
-		<div class="column is-7">
-			{{task.description}}
+	<task-box>
+		<div class="columns">
+			<div class="column is-7">
+				{{task.description || 'Without description'}}
+			</div>
+			<div class="column">
+				<chronometer-timer :elapsedTime="task.elapsedTime" />
+			</div>
 		</div>
-		<div class="column">
-			<ChronometerTimer :elapsedTime="task.elapsedTime" />
-		</div>
-	</div>
+	</task-box>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
 import ChronometerTimer from "./ChronometerTimer.vue";
 import ITask from "@/interfaces/ITask";
+import TaskBox from "./TaskBox.vue";
 
 export default defineComponent({
 	name: 'TaskItem',
-	components: { ChronometerTimer },
+	components: { ChronometerTimer, TaskBox },
 	props: {
 		task: {
 			type: Object as PropType<ITask>,
@@ -25,9 +28,3 @@ export default defineComponent({
 	}
 })
 </script>
-
-<style scoped>
-.box {
-	background: #FAF0CA;
-}
-</style>
