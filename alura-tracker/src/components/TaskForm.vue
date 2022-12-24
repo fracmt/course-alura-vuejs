9c580f087d3817de9309a1e8eba79b2c6b48914d
Francisco Torres <fracmt@gmail.com>
2022-12-24 00:48:06 +0000

    List of projects using Vuex

--- alura-tracker/src/components/TaskForm.vue
+++ alura-tracker/src/components/TaskForm.vue
@@ -1,8 +1,18 @@
 <template>
 	<div class="box form-task">
 		<div class="columns">
-			<div class="column is-8" role="form" aria-label="Form to create new tasks">
+			<div class="column is-5" role="form" aria-label="Form to create new tasks">
 				<input type="text" class="input" placeholder="What task will you start?" v-model="taskDescription">
+			</div>
+			<div class="column is-3">
+				<div class="select">
+					<select v-model="taskIdProject">
+						<option value="">Select the project</option>
+						<option :value="project.id" v-for="project in projects" :key="project.id">
+							{{ project.name }}
+						</option>
+					</select>
+				</div>
 			</div>
 			<div class="column">
 				<div class="is-flex is-align-items-center is-justify-content-space-between">
@@ -14,8 +24,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
 import TaskTimer from './TaskTimer.vue'
+import { useStore } from 'vuex';
+
+import { key } from '@/store'
 
 export default defineComponent({
 	name: 'TaskForm',
@@ -25,16 +38,24 @@ export default defineComponent({
 	},
 	data() {
 		return {
-			taskDescription: ''
+			taskDescription: '',
+			taskIdProject: ''
 		}
 	},
 	methods: {
 		stopTask(elapsedTime: number): void {
 			this.$emit('toSaveTask', {
-				elapsedTime: elapsedTime, 
-				description: this.taskDescription
+				elapsedTime: elapsedTime,
+				description: this.taskDescription,
+				project: this.projects.find(item => item.id == this.taskIdProject)
 			})
 			this.taskDescription = '';
+		}
+	},
+	setup() {
+		const store = useStore(key)
+		return {
+			projects: computed(() => store.state.projects)
 		}
 	}
 })
